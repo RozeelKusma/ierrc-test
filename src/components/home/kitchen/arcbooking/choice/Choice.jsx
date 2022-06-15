33b45febf1e590110c3d95b7fdee55e5e1ba2515
RozeelKusma <kusmarozeel@gmail.com>
2022-06-15 14:25:01 +0545

    changes made with booking page and added some content to ramailo after school page

--- src/components/home/kitchen/arcbooking/choice/Choice.jsx
+++ src/components/home/kitchen/arcbooking/choice/Choice.jsx
@@ -1,51 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './choice.css'
-import PopUp2 from './popup2/PopUp2';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./choice.css";
+import PopUp2 from "./popup2/PopUp2";
 
 export default class Arcane extends React.Component {
-    state = {
-      seen: false
-    };
-  
-    togglePop = () => {
-      this.setState({
-        seen: !this.state.seen
-      });
-    };
-  
-    render() {
-return (
-  <div className='choice_main'>
-      <div className="choice_container">
-          <h1 className="c_title">
-              Select your choice
-          </h1>
+  state = {
+    seen: false,
+  };
+
+  togglePop = () => {
+    this.setState({
+      seen: !this.state.seen,
+    });
+  };
+
+  render() {
+    return (
+      <div className="choice_main">
+        <div className="choice_container">
+          <h1 className="c_title">Select your choice</h1>
           <div className="choice_s">
-             
-              <div className="c1" onClick={this.togglePop}>
-                  Solo Adventure
-              </div>
-              
-              <Link to='/arcane-quest/booking' className='c_link'>
-              <div className="c2">
-                  Group of Friends
-              </div>
-              </Link>
-              <Link to='/arcane-quest/booking' className='c_link'>
-              <div className="c3">
-                  Family
-              </div>
-              </Link>
-              <Link to='/arcane-quest/booking' className='c_link'>
-              <div className="c4">
-                  Office Team
-              </div>
-              </Link>
+            <div className="c1" onClick={this.togglePop}>
+              Solo Adventure
+            </div>
+            <Link to="/arcane-quest/booking" className="c_link">
+              <div className="c2">Group of Friends</div>
+            </Link>
+            <Link to="/arcane-quest/booking" className="c_link">
+              <div className="c3">Family</div>
+            </Link>
+            <Link to="/arcane-quest/booking" className="c_link">
+              <div className="c4">Office Team</div>
+            </Link>
           </div>
+        </div>
+        {this.state.seen ? (
+          <PopUp2 toggle={this.togglePop} className="pop" />
+        ) : null}
       </div>
-      {this.state.seen ? <PopUp2 toggle={this.togglePop} className="pop"/> : null}
-  </div>
-);
-}
+    );
+  }
 }
